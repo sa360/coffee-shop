529280fbc155ac6e467626e2ab64fe88395ee7a9
Suzanne Adams <brainstation@BrainStations-MacBook-Air-2.local>
2016-02-29 13:59:58 -0500

    localhost to IP

--- app/services/api.srv.js
+++ app/services/api.srv.js
@@ -3,7 +3,7 @@ app.service('api', ApiService);
 function ApiService($http) {
   this.http = $http;
   //constants
-  this.BASE_URL = 'http://52.37.123.184/';
+  this.BASE_URL = 'http://52.37.123.184';
 }
 ApiService.prototype.request = function(endpoint,data,method) {
   //build request
