7be682b83e687c9923b95a55641e8b4172692175
Suzanne Adams <brainstation@BrainStations-MacBook-Air-2.local>
2016-03-01 11:57:08 -0500

    enviro changes

--- api/config/env_prod.js
+++ api/config/env_prod.js
@@ -1,1 +1,1 @@
-module.exports = {port:80,host:'http://52.37.123.184'};
+module.exports = {port:80,host:'http://52.37.123.184',database:'production'};
--- api/models/index.js
+++ api/models/index.js
@@ -4,7 +4,13 @@ var fs        = require('fs');
 var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
-var env       = process.env.NODE_ENV || 'development';
+try{
+  var env = require('./../config/env_dev').database;
+}
+catch(err){
+  var env = require('./../config/env_prod').database;
+}
+// var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
